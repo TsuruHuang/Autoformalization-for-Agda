6067eff96c0acdbcccdadfbdcafcd3424aed0b5c
Pei Huang <peih@chalmers.se>
2025-03-04 19:50:23 +0100

    Data Formating.

--- data_convertion.py
+++ data_convertion.py
@@ -3,14 +3,11 @@ import random
 
 random.seed(42)
 
-# 读取 JSON 文件
 with open("E:\Project\data\exx-gflean.jsonl", "r", encoding="utf-8") as f:
     data = [json.loads(line) for line in f.readlines()]
 
-# 存储转换后的数据
 formatted_data = []
 
-# 遍历每条数据
 for entry in data:
     for lang_key, lang_values in [
         ("InformathEng", "English"),
@@ -31,15 +28,12 @@ for entry in data:
                         "output": formal_expr
                     })
 
-# 随机打乱数据
 random.shuffle(formatted_data)
 
-# 按 8.5:1.5 划分
 split_idx = int(0.85 * len(formatted_data))
 train_set = formatted_data[:split_idx]
 test_set = formatted_data[split_idx:]
 
-# 保存训练集和测试集
 with open("train_all_eng.json", "w", encoding="utf-8") as f:
     json.dump(train_set, f, ensure_ascii=False, indent=2)
 
