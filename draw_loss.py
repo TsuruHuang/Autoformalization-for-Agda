08b7f1aa915ae865ae80481a0ebd3759e3b2a391
Pei Huang <peih@chalmers.se>
2025-05-29 07:15:08 +0200

    Update smooth loss.

--- draw_loss.py
+++ draw_loss.py
@@ -2,14 +2,20 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
+def smooth(series, window_size=10):
+    return series.rolling(window=window_size, min_periods=1).mean()
+
 def plot_loss_curves(log_files):
     labels = [os.path.splitext(os.path.basename(f))[0] for f in log_files]
     plt.figure(figsize=(8, 5))
 
     for f, label in zip(log_files, labels):
         df = pd.read_json(f, lines=True)
+        # df = df[df['loss'] > 1e-6]
+        df['loss'] = smooth(df['loss'], window_size=5)
         plt.plot(df['current_steps'], df['loss'], label=label)
 
+    plt.yscale('log')
     plt.xlabel('Training Steps')
     plt.ylabel('Loss')
     plt.title('Step - Loss Curves')
